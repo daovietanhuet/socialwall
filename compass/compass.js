66219ec951e4497227cee5515a5736811c410e0f
daovietanhuet <daovietanhsky@gmail.com>
2019-05-29 09:51:17 +0700

    compass test

--- compass/compass.js
+++ compass/compass.js
@@ -1,12 +1,18 @@
+import { setInterval } from "timers";
+
 var compass = new Vue({
     el: '#compass',
     data: {
         alpha: null,
         heading: null,
+    },
+    methods: {
+        
+    },
+    created: () => {
+        setInterval(() => {this.heading = this.alpha}, 500);
     }
 });
-
-setInterval(() => {compass.heading = compass.alpha}, 50);
 
 window.addEventListener("deviceorientation", function(e) {
     compass.alpha = Math.ceil(e.alpha);
