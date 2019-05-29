99b5c0439107ca465e1b7cce2a74c63529114e7f
daovietanhuet <daovietanhsky@gmail.com>
2019-05-29 10:44:42 +0700

    using alpha

--- compass/compass.js
+++ compass/compass.js
@@ -2,7 +2,6 @@ var compass = new Vue({
     el: '#compass',
     data: {
         alpha: 0,
-        heading: 0
     }
 });
 
@@ -12,9 +11,3 @@ window.addEventListener("deviceorientation", function(e) {
         compass.alpha = Math.ceil(e.webkitCompassHeading); //iOS non-standard
     }
 }, false);
-
-function smoothHeading() {
-    setInterval(() => {compass.heading = compass.alpha;}, 50)
-}
-
-smoothHeading();
