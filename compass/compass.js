3fc326ba92c84dad4a47f5dd642af67fbad88667
daovietanhuet <daovietanhsky@gmail.com>
2019-05-29 16:05:45 +0700

    sua compass

--- compass/compass.js
+++ compass/compass.js
@@ -13,6 +13,7 @@ window.addEventListener("deviceorientation", function(e) {
     if (typeof e.webkitCompassHeading !== "undefined") {
         compass.alpha = Math.abs(Math.ceil(e.webkitCompassHeading) - compass.alpha) <= 1 ? compass.alpha : Math.ceil(e.webkitCompassHeading); //iOS non-standard
     }
-    compass.dir = direction[(compass.alpha - compass.alpha % 90) / 90 - 1] + 
-        compass.alpha % 90 !== 0 ? direction[((compass.alpha - compass.alpha % 90) / 90) % 4] : ''
+    heading = 360 - compass.alpha; 
+    compass.dir = direction[((heading - heading % 90) / 90) % 4] + 
+        (heading % 90 !== 0 ? direction[((heading - heading % 90) / 90 +1) % 4] : '')
 }, false);
