c3e4b3aefdd77339d38fbb472fb22ab9d0eab884
daovietanhuet <daovietanhsky@gmail.com>
2019-05-29 15:21:46 +0700

    make compass smooth

--- compass/compass.js
+++ compass/compass.js
@@ -8,6 +8,6 @@ var compass = new Vue({
 window.addEventListener("deviceorientation", function(e) {
     compass.alpha = Math.ceil(e.alpha);
     if (typeof e.webkitCompassHeading !== "undefined") {
-        compass.alpha = Math.ceil(e.webkitCompassHeading); //iOS non-standard
+        compass.alpha = Math.abs(Math.ceil(e.webkitCompassHeading) - compass.alpha) <= 1 ? compass.alpha : Math.ceil(e.webkitCompassHeading); //iOS non-standard
     }
 }, false);
